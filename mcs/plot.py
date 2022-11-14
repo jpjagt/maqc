a211a3cfc54dfdd52d3003875b96fbb6fbcd0123
jeroen <jpjagt@pm.me>
2022-11-14 13:51:00 +0100

    add plot fns

--- mcs/plot.py
+++ mcs/plot.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import geopandas as gpd
-import numpy as np
 
-from mcs.constants import ASSETS_DIR, BINSIZES
+from mcs.constants import ASSETS_DIR
 
 amsterdam_geojson = gpd.read_file(
     str(ASSETS_DIR / "amsterdam_neighbourhoods.geojson")
@@ -41,4 +40,30 @@ def plot_line(df, variable, yscale="linear", xlabel=None, ylabel=None):
     if ylabel:
         ax.set_ylabel(ylabel)
 
-    plt.show()
+
+def plot_timestamps_of_mit_sensor(df, name=None):
+    start = df.timestamp.min() + pd.DateOffset(minutes=-5)
+    end = df.timestamp.max() + pd.DateOffset(minutes=5)
+
+    df_times = pd.DataFrame(
+        index=pd.date_range(start, end, freq="S"), columns=["sensor"]
+    )
+    df_times["sensor"] = False
+    df_times.loc[df.timestamp, "sensor"] = True
+
+    df_times.astype(int).plot(alpha=0.5)
+
+    if name:
+        plt.title(f"recorded timestamps for {name}")
+
+
+def plot_col_of_multiple_sensors(name2series, colname):
+    df = pd.DataFrame(name2series)
+    start = df.index.min() + pd.DateOffset(minutes=-5)
+    end = df.index.max() + pd.DateOffset(minutes=5)
+
+    index = pd.date_range(start, end, freq="S")
+    df = df.reindex(index)
+
+    df.astype(float).plot(alpha=0.5)
+    plt.ylabel(colname)
