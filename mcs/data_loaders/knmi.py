a9804c9f2465f43ffa217bff76f58e9569e2e587
jeroen <jpjagt@pm.me>
2022-12-01 14:05:15 +0100

    adapt knmi code to hourly data

--- mcs/data_loaders/knmi.py
+++ mcs/data_loaders/knmi.py
@@ -1,36 +1,104 @@
-from mcs.constants import KNMI_DATA_DIR
-from mcs.constants import KNMI_START_DATE
+from mcs.constants import (
+    KNMI_DATA_DIR,
+    EXPERIMENT_START_DATE,
+    EXPERIMENT_END_DATE,
+)
 import os
 import pandas as pd
+
+colcode2colname = {
+    "# STN": "station",
+    "YYYYMMDD": "date",
+    "HH": "hour",
+    "DD": "wind_direction",
+    "FH": "wind_speed_hourly",
+    "FF": "wind_speed_last10min",
+    "FX": "wind_max_gust",
+    "T": "temperature",
+    "T10N": "temperature_min",
+    "TD": "temperature_dew_point",
+    "SQ": "sunshien_duration",
+    "Q": "global_radiation",
+    "DR": "precipitation_duration",
+    "RH": "precipitation_hourly",
+    "P": "air_pressure",
+    "VV": "horizontal_visibility",
+    "N": "cloud_cover",
+    "U": "relative_humidity",
+    "WW": "weather_code",
+    "IX": "indicator_present_weather_code",
+    "M": "is_foggy",
+    "R": "is_raining",
+    "S": "is_snowing",
+    "O": "is_thundering",
+    "Y": "ice_formation",
+}
+
+# old col names for hourly data:
+# col_names = [
+#     "Station",
+#     "Date",
+#     "Avg_Wind_Direction",
+#     "Avg_Windspeed",
+#     "Daily_Avg_Windspeed",
+#     "Max_Windspeed",
+#     "Time_Max_Windspeed",
+#     "Min_Windspeed",
+#     "Time_Min_Windspeed",
+#     "Max_Gust",
+#     "Time_Max_Gust",
+#     "Avg_Temp",
+#     "Min_Temp",
+#     "Time_Min_Temp",
+#     "Max_Temp",
+#     "Time_Max_Temp",
+#     "Min_Temp_10cm",
+#     "Time_Min_Temp_10cm",
+#     "Sun_Duration",
+#     "Pct_Max_Sun",
+#     "Radiation",
+#     "Precip_Duration",
+#     "Precip_Amt",
+#     "Max_Precip",
+#     "Time_Max_Precip",
+#     "Avg_Sea_Pressure",
+#     "Max_Sea_Pressure",
+#     "Time_Max_Sea_Pressure",
+#     "Min_Sea_Pressure",
+#     "Time_Min_Sea_Pressure",
+#     "Min_Visibility",
+#     "Time_Min_Visibility",
+#     "Max_Visibility",
+#     "Time_Max_Visibility",
+#     "Avg_Cloud_Cover",
+#     "Avg_Atmos_Humidity",
+#     "Max_Humidity",
+#     "Time_Max_Humidity",
+#     "Min_Humidity",
+#     "Time_Min_Humidity",
+#     "Evapotranspiration",
+# ]
 
 
 class KNMIDataLoader(object):
     def load_data(self):
-        fpath = os.path.join(KNMI_DATA_DIR, "etmgeg_240.txt")
-        with open(fpath, 'r') as f:
-            df = pd.read_csv(f, skiprows=50, sep=",")
-            df.columns = df.columns.str.strip()
+        fname = "uurgeg_240_2021-2030.txt"
+        fpath = os.path.join(KNMI_DATA_DIR, fname)
+        df = pd.read_csv(fpath, skiprows=30, sep=",")
+        df.columns = df.columns.str.strip().map(colcode2colname)
 
-        col_names = ["Station", "Date", "Avg_Wind_Direction", "Avg_Windspeed", "Daily_Avg_Windspeed", "Max_Windspeed",
-                    "Time_Max_Windspeed", "Min_Windspeed", "Time_Min_Windspeed", "Max_Gust", "Time_Max_Gust",
-                    "Avg_Temp", "Min_Temp", "Time_Min_Temp", "Max_Temp", "Time_Max_Temp", "Min_Temp_10cm", "Time_Min_Temp_10cm",
-                    "Sun_Duration", "Pct_Max_Sun", "Radiation", "Precip_Duration", "Precip_Amt", "Max_Precip", "Time_Max_Precip",
-                    "Avg_Sea_Pressure", "Max_Sea_Pressure", "Time_Max_Sea_Pressure", "Min_Sea_Pressure", "Time_Min_Sea_Pressure",
-                    "Min_Visibility", "Time_Min_Visibility", "Max_Visibility", "Time_Max_Visibility", "Avg_Cloud_Cover",
-                    "Avg_Atmos_Humidity", "Max_Humidity", "Time_Max_Humidity", "Min_Humidity", "Time_Min_Humidity", "Evapotranspiration"]
+        # convert date and time information to DateTime column
+        df["timestamp"] = pd.to_datetime(
+            df["date"].astype(str)
+            + " "
+            + df["hour"].astype(str).str.zfill(2).replace("24", "00"),
+            format="%Y%m%d %H",
+        )
 
-        df.columns = col_names
-        # convert to DateTime object
-        df['Date'] = pd.to_datetime(df['Date'], format='%Y%m%d')
+        # set timestamp as index
+        df = df.set_index("timestamp")
 
-        # filter out data before 2021
-        df = df.loc[(df['Date'] >= KNMI_START_DATE)]
-
-        # filter out columns the most relevant to our research
-        df = df.drop(columns=["Sun_Duration", "Pct_Max_Sun", "Radiation", "Precip_Duration", "Precip_Amt",
-                              "Max_Precip", "Time_Max_Precip", "Min_Visibility", "Time_Min_Visibility",
-                              "Max_Visibility",
-                              "Time_Max_Visibility", "Avg_Cloud_Cover", "Evapotranspiration"])
-
+        # only select data which was within experiment time period
+        df = df.loc[EXPERIMENT_START_DATE:EXPERIMENT_END_DATE]
 
         return df
--- scripts/fetch_knmi_data.py
+++ scripts/fetch_knmi_data.py
@@ -5,15 +5,19 @@ from io import BytesIO
 
 
 def get_data_for():
-    url = "https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/daggegevens/etmgeg_240.zip"
-    response = requests.get(url)
-    #unzip that bad boy
-    zippy = zipfile.ZipFile(BytesIO(response.content))
-    KNMI_DATA_DIR.mkdir(exist_ok=True)
-    fpath = KNMI_DATA_DIR
-    zippy.extractall(fpath)
+    urls = [
+        "https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/daggegevens/etmgeg_240.zip",
+        "https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/uurgegevens/uurgeg_240_2021-2030.zip",
+    ]
 
-if __name__ == '__main__':
+    for url in urls:
+        response = requests.get(url)
+        # unzip that bad boy
+        zippy = zipfile.ZipFile(BytesIO(response.content))
+        KNMI_DATA_DIR.mkdir(exist_ok=True)
+        fpath = KNMI_DATA_DIR
+        zippy.extractall(fpath)
+
+
+if __name__ == "__main__":
     get_data_for()
-
-
