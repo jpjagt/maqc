78f6fc8b0307c2b59f5504e190759d0185cc278e
Jonas van der Ham <jonasvdham@gmail.com>
2022-12-19 15:12:23 +0100

    add format option to plot and ufp.org

--- mcs/plot.py
+++ mcs/plot.py
@@ -46,6 +46,7 @@ def plot_line(
     ylabel=None,
     legend=None,
     title=None,
+    datefmt=None,
     outliers=None,
     outfile=None,
 ):
@@ -83,8 +84,10 @@ def plot_line(
         ax.legend(legend)
     if title:
         ax.set_title(title)
-
-    if outfile != None:
+    if datefmt:
+        ax.xaxis.set_major_formatter(mdates.DateFormatter(datefmt))
+        ax.xaxis.set_minor_formatter(mdates.DateFormatter(datefmt))
+    if outfile:
         fig.savefig(str(ROOT_DIR / ("exports/" + outfile + ".svg")))
     fig.show()
 
